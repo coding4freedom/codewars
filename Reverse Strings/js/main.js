b117109d44b930bd24a9b17ae52dac8c30b39c89
coding4freedom <chawkins2010@hotmail.com>
2022-04-28 10:56:16 -0500

    Reverse Strings first commit

---
+++ Reverse Strings/js/main.js
@@ -0,0 +1,13 @@
+/*
+Complete the solution so that it reverses the string passed into it.
+
+example:
+'world'  =>  'dlrow'
+
+*/
+
+function solution(str){
+    let arr = [];
+    arr = str.split('').reverse();
+    return  str = arr.join('')
+  }
