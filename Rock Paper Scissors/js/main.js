a0b05e12d7fad6f38b320bfab7f119151ff1aef5
coding4freedom <chawkins2010@hotmail.com>
2022-04-29 21:45:22 -0500

    Rock Paper Scissors try 2

--- Rock Paper Scissors/js/main.js
+++ Rock Paper Scissors/js/main.js
@@ -1,5 +1,6 @@
 
-/* Let's play! You have to return which player won! In case of a draw return Draw! */
+/* Let's play! You have to return which player won! In case of a 
+draw return Draw! */
 
 /*
 rps('scissors','paper') // Player 1 won!
@@ -7,13 +8,22 @@ rps('scissors','rock') // Player 2 won!
 rps('paper','paper') // Draw!
 */
 
+
 const rps = (p1, p2) => {
-    if (p1 === p2) return "Draw!";
-    var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
-    if (p2 === rules[p1]) {
-      return "Player 1 won!";
+
+    if((p1 === 'scissors' && p2 === 'paper') 
+        || (p1 === 'paper' && p2 === 'rock')
+        || (p1 === 'rock' && p2 === 'scissors') 
+    ){
+        console.log('Player 1 wins!')
+    }else if (
+        (p2 === 'scissors' && p1 === 'paper') 
+        || (p2 === 'paper' && p1 === 'rock')
+        || (p2 === 'rock' && p1 === 'scissors')
+    ){
+        console.log('Player 2 wins!')
+    }else {
+        console.log('Draw!')
     }
-    else {
-      return "Player 2 won!";
-    }
-  };
+
+}    
