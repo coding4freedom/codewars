fd10bcc255abcc7be1a1be8ca2a061d49dedd021
coding4freedom <chawkins2010@hotmail.com>
2022-05-02 21:13:16 -0500

    String repeat first commit

---
+++ String repeat/js/main.js
@@ -0,0 +1,17 @@
+
+/*
+Write a function called repeatStr which repeats the 
+given string string exactly n times.
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+*/
+
+function repeatS (n, s) {
+
+}
+
+function repeatStr (n, s) {  
+    s = s.repeat(n)
+    return s;
+}
