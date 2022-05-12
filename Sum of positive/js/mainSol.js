95f30945b020e9bc04324dc46ed82ec8fd631ec3
coding4freedom <chawkins2010@hotmail.com>
2022-05-11 19:36:41 -0500

    Sum of positive try 2

--- Sum of positive/js/mainSol.js
+++ Sum of positive/js/mainSol.js
@@ -9,12 +9,14 @@ Note: if there is nothing to sum, the sum is default to 0.
 */
 
 function pos(arr){
-    let result = arr.reduce((a, b) => {
-        return Math.sign(b) === 1 ? a + b: a + 0;
-    },0)
-    return result;
+
 }
 
-let nums = [1,-4,7,12];
-
-console.log(pos(nums));
+function positiveSum(arr) {
+  
+    let sum = arr.reduce((acc, curr) => {
+     return Math.sign(curr) === 1 ? acc + curr : acc + 0;
+      
+    }, 0)
+    return sum;
+}
