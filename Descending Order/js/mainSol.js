2d5caf972658296afcd3d0293dc02cc5e1dbee31
coding4freedom <chawkins2010@hotmail.com>
2022-06-07 21:53:13 -0500

    Descending Order try 2

--- Descending Order/js/mainSol.js
+++ Descending Order/js/mainSol.js
@@ -9,8 +9,16 @@ Input: 42145 Output: 54421
 */
 
 function descendOrder(n){
-    let newN = String(n).split('').sort((a,b) => b - a).join('');
-    return parseInt(newN)
+
 }
 
-console.log(descendOrder(42145))
+
+function descendingOrder(n){
+    //...
+    let newN =  n
+      .toString()
+      .split('')
+      .sort((a,b) => b - a)
+      .join('')
+    return Number(newN)
+}
