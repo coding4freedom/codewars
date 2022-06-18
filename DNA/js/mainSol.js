9363d37cc0bf927167d81883b72a4ef1ebd68a5d
coding4freedom <chawkins2010@hotmail.com>
2022-06-17 22:45:48 -0500

    DNA try 2

--- DNA/js/mainSol.js
+++ DNA/js/mainSol.js
@@ -1,18 +1,20 @@
+
 /*
 In DNA strings, symbols "A" and "T" are complements of each other, 
 as "C" and "G". You function receives one side of the DNA 
 (string, except for Haskell); you need to return the other 
 complementary side. 
-DNA strand is never empty or there is no DNA at all (again, 
-    except for Haskell).
+DNA strand is never empty or there is no DNA at all (again, except for Haskell).
 
 "ATTGC" --> "TAACG"
 "GTAT" --> "CATA"
 */
 
 function dnaStrand(dna){
-    let strands = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
-    return dna.split('').map(strand => strands[strand]).join('')
+
 }
 
-console.log(dnaStrand("CATA"))
+function DNAStrand(dna){
+    let pairs = {'A':'T','T':'A','C':'G','G':'C'};
+    return dna.split('').map(function(v){ return pairs[v] }).join('');
+  }
