1db4b84f5f917e1f9e4e3c3491b48f121e7f0129
coding4freedom <chawkins2010@hotmail.com>
2022-06-27 20:17:04 -0500

    Isogram 2 try

--- Isograms/js/mainSol.js
+++ Isograms/js/mainSol.js
@@ -14,5 +14,9 @@ Example: (Input --> Output)
 */
 
 function isIso(str){
-    return !str || (str.length === new Set(str.toLowerCase()).size)
+
+}
+
+function isIsogram (str) {
+    return !str || (str.length === new Set(str.toLowerCase()).size);
 }
