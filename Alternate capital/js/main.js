b11d97800a73e2427876cad74fe2a53ec39a824f
coding4freedom <chawkins2010@hotmail.com>
2022-07-07 22:56:10 -0500

    Alternate capital first commit

---
+++ Alternate capital/js/main.js
@@ -0,0 +1,22 @@
+/*
+Given a string, capitalize the letters that occupy even 
+indexes and odd indexes separately, and return as shown below. 
+Index 0 will be considered even.
+
+For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. 
+See test cases for more examples.
+
+The input will be a lowercase string with no spaces.
+
+*/
+
+function altCaps(s){
+
+}
+
+function capitalize(s){
+    let even = s.split('').map((s, index) => index % 2 === 0? s.toUpperCase(): s).join('')
+    let odd =  s.split('').map((s, index) => index % 2 === 0? s: s.toUpperCase()).join('')
+    
+    return [even, odd];
+};
