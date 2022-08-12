a3a49a2af86ff0aa580b5ccae7b5facbae1ad8cc
coding4freedom <chawkins2010@hotmail.com>
2022-08-11 19:35:04 -0500

    Reverse words try 5

--- Reverse words/js/main4.js
+++ Reverse words/js/main4.js
@@ -9,7 +9,6 @@ Examples
 */
 
 function revWords(str){
-    return str.split('').reverse().join('').split(' ').reverse().join(' ')
+    return str.split('').reverse().join("").split(' ').reverse().join(" ")
 }
-
 console.log(revWords("This is an example!"))
