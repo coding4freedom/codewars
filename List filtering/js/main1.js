385fb169dfa4a330b98740b6ef914d89be9442eb
coding4freedom <chawkins2010@hotmail.com>
2022-08-13 21:20:52 -0500

    List filtering try 2

--- List filtering/js/main1.js
+++ List filtering/js/main1.js
@@ -1,3 +1,4 @@
+
 /*
 In this kata you will create a function that takes 
 a list of non-negative integers and strings and returns 
@@ -6,7 +7,7 @@ a new list with the strings filtered out.
 */
 
 function listFilter(l) {
-    return l.filter(fact => Number.isInteger(fact))
+    return l.filter(num => Number.isInteger(Number.parseInt(num)))
 }
 
 console.log(listFilter(['a','k','k','i',1,5,0]))
