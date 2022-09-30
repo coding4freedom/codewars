6ecb09cdd018643820cd844dee21efbeff8b086a
coding4freedom <chawkins2010@hotmail.com>
2022-09-29 20:40:26 -0500

    Most digits try 2

--- Most digits/js/mainSol.js
+++ Most digits/js/mainSol.js
@@ -8,8 +8,10 @@ If two numbers in the argument array have the same number
 */
 
 function findLong(array){
-    let arr = array.sort((a,b) => b - a)
-    return arr[0]
+
 }
 
-console.log(findLong([5214, 2, 44587, 238, 874]))
+function findLongest(array){
+    return array.reduce((gather, first) => `${first}`.length > `${gather}`.length ? first: gather)
+}
+console.log(findLongest([5214, 2, 4458, 238, 874]))
