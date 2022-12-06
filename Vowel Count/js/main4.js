80243a9bed97cf398e17e3768748f9528ad68070
coding4freedom <chawkins2010@hotmail.com>
2022-12-06 17:59:10 -0600

    Vowel Count try 4

--- Vowel Count/js/main4.js
+++ Vowel Count/js/main4.js
@@ -1,4 +1,3 @@
-
 /*
 
 Return the number (count) of vowels in the given string.
@@ -7,16 +6,21 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
 The input string will only consist of lower case letters and/or spaces.
 
-'sample'
-'a', 'e', 'i', 'o', 'u'
 */
 
-function getCoun(str){
+function getCount(str) {
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+
     let count = 0;
-    let vow = ['a', 'e', 'i', 'o', 'u']
 
-    Array.from(str).map( s => vow.includes(s) ? count++ : false)
+    Array.from(str).map(s =>{
+        if(vowels.includes(s)){
+            count++
+        }
+        return count
+    })
+
     return count
-}
+}   
 
-console.log(getCoun('sample'))
+console.log(getCount('superallknowingfamily'))
--- Vowel Count/js/mainSol.js
+++ Vowel Count/js/mainSol.js
@@ -7,7 +7,13 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
 The input string will only consist of lower case letters and/or spaces.
 
+
+'a', 'e', 'i', 'o', 'u'
 */
+
+function getCoun(str){
+
+}
 
 function getCount(str) {
     var vowelsCount = 0;
