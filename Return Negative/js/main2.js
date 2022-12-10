2a5583d5759f8260ea8c9cff05b72da0ccaed283
coding4freedom <chawkins2010@hotmail.com>
2022-12-10 17:18:18 -0600

    Return Negative try 3

--- Return Negative/js/main2.js
+++ Return Negative/js/main2.js
@@ -1,4 +1,3 @@
-
 /*
 In this simple assignment you are given a number and have 
 to make it negative. But maybe the number is already negative?
@@ -10,8 +9,9 @@ makeNegative(0);    // return 0
 makeNegative(0.12); // return -0.12
 */
 
+
 function makeNeg(num) {
-    return Math.sign(num) === 1 || Math.sign(num) === 0 ? num : num = -(num)
+    return Math.sign(num) ? -(num) : num
 }
 
 console.log(makeNeg(0))
