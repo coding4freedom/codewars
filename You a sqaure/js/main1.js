4ae4fa86f6473565d0d99a952aabd4aebc306ac2
coding4freedom <chawkins2010@hotmail.com>
2022-12-23 20:35:36 -0600

    You a square try 2

--- You a sqaure/js/main1.js
+++ You a sqaure/js/main1.js
@@ -18,7 +18,7 @@ Examples
 
 */
 function square(n){
-    return Number.isInteger(Math.sqrt(n))
+    return Number.isInteger(Math.sqrt(n))? true : false;
 }
 
-console.log(square(3))
+console.log(square(5))
