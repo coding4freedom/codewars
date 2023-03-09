27057684362b2fe421e8f71998965d18026b052d
coding4freedom <chawkins2010@hotmail.com>
2023-03-08 20:04:34 -0600

    Remove String Spaces sol

---
+++ Remove String Spaces/js/mainSol.js
@@ -0,0 +1,13 @@
+
+// Simple, remove the spaces from the string, then return the resultant string.
+
+
+function noSpace(x){
+
+}
+
+// solution
+
+function noSpace(x){
+    return x.split(' ').join('')
+  }
