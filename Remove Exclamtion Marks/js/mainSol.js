e54fcdceb0c8b07d0a565616fbf9a42f9e75cbb5
coding4freedom <chawkins2010@hotmail.com>
2023-05-01 22:03:02 -0500

    Remove Exclamation Marks

---
+++ Remove Exclamtion Marks/js/mainSol.js
@@ -0,0 +1,14 @@
+
+/*
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+*/
+
+function removeExclamMarks(s) {
+  return '';
+}
+
+function removeExclamationMarks(s) {
+  return s.split('!').join('');
+}
