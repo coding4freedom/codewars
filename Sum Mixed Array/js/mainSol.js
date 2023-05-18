973190342cc8a2d0e0704f831f3887965560242c
coding4freedom <chawkins2010@hotmail.com>
2023-05-17 21:18:35 -0500

    Sum Mixed Array Sol

---
+++ Sum Mixed Array/js/mainSol.js
@@ -0,0 +1,15 @@
+
+/*
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+
+
+function sumMix(x){
+
+}
+
+function sumMixArr(x){
+    return x.map(a => +a).reduce((a, b) => a + b);
+}
